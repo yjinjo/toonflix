6b5d3588039ee910b041fc1c4f5e60a9c8f7dddc
Youngjin Jo <yjinjo@berkeley.edu>
2023-05-16 20:53:00 +0900

    #4. Layout of AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -9,6 +10,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
